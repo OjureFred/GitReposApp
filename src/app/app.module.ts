045002226ce77a116bc1f761345886d93f72388d
Fred Ojure <fredojure@hotmail.com>
2020-06-23 10:04:41 +0300

    Generated About component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitReposComponent } from './git-repos/git-repos.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitReposComponent
+    GitReposComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
