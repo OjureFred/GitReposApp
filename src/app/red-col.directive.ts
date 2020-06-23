aebfab1402ca43f36f5dbd265de61df16dcfc86e
Fred Ojure <fredojure@hotmail.com>
2020-06-23 17:37:45 +0300

    Coded the directive

--- src/app/red-col.directive.ts
+++ src/app/red-col.directive.ts
@@ -1,10 +1,13 @@
 import { Directive } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appRedCol]'
 })
 export class RedColDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { 
+    this.elem.nativeElement.style.textDocoration = 'underline';
+  }
 
 }
