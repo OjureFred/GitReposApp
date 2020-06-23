681f080f0cfe6fbf895d649177128f67c3c7ac4c
Fred Ojure <fredojure@hotmail.com>
2020-06-23 15:39:53 +0300

    Added code to git repository component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { GitReposComponent } from './git-repos/git-repos.component';
+import { GitRepositoryComponent } from './git-repository/git-repository.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component'
 
 
 const routes: Routes = [
   { path: 'gitrepos', component: GitReposComponent },
+  { path: 'gitlist', component: GitRepositoryComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', component: NotFoundComponent },
   { path: ' ', redirectTo: '/gitrepos', pathMatch: 'full'}
--- src/app/git-repository/git-repository.component.ts
+++ src/app/git-repository/git-repository.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GitRequestService } from './../git-http/git-request.service';
+import { User } from '../user';
+import { Repos } from '../repos';
 
 @Component({
   selector: 'app-git-repository',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./git-repository.component.css']
 })
 export class GitRepositoryComponent implements OnInit {
+  user: User;
+  repos: Repos;
 
-  constructor() { }
+  constructor(private httpService: GitRequestService, private repoService: GitRequestService) { }
+
+  getTheUser(name: string) {
+    this.httpService.getUser(name).then(
+      (result) => {
+        this.user = this.httpService.gitUser;
+        console.log(this.user);
+      },
+      (error) =>{
+        console.log(error);
+      }
+    );
+
+    this.repoService.getRepos(name).then(
+        (results) => {
+        this.repos = this.repoService.gitRepos;
+          console.log (this.repos);
+        },
+        (error)=>{
+          console.log(error);
+        }
+      );
+  }
 
   ngOnInit(): void {
+    //this.getTheUser('OjureFred');
   }
 
 }
