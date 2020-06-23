74ac2b999658754eee57341112f9b6dfbb042d47
Fred Ojure <fredojure@hotmail.com>
2020-06-23 18:08:34 +0300

    Changed production to true in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 
 export const environment = {
-  production: false,
+  production: true,
   apiToken: '5e1054156642f80729b3355d5a2b5af5e30ecab4'
 };
 
