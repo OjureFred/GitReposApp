441f09fd1e713c431ad9d5206eedf5006b595161
Fred Ojure <fredojure@hotmail.com>
2020-06-23 10:19:28 +0300

    Added code to About component and reated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { GitReposComponent } from './git-repos/git-repos.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'gitrepos', component: GitReposComponent },
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
